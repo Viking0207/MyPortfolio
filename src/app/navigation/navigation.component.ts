3fc42588cc262c9a0980dad4a83d3007d10489f8
Viking0207 <fickyfebryan0@gmail.com>
2024-09-18 10:08:37 +0700

    Penambahan fitur navigasi
    Menambahkan fungsi navbar di navigasi.ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  openSection: string | null = null;
+
+  toggleSection(section: string) {
+    this.openSection = this.openSection === section ? null : section;
+    
+    console.log(`Clicked section: ${section}`);
+    console.log(`Open section after toggle: ${this.openSection}`);
+  }
 
 }
