f81899bc0ff78b5f335d897f15bf206392cef4df
Viking0207 <fickyfebryan0@gmail.com>
2024-10-24 12:09:45 +0700

    refactor(animation): adding an animation of transition on this web

--- src/app/first-page/body/body.component.ts
+++ src/app/first-page/body/body.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
-export class BodyComponent {
+export class BodyComponent implements AfterViewInit {
+
+  // Mendapatkan referensi tombol dari template
+  @ViewChild('hireBtn') hireBtn!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    // Menambahkan listener untuk mendeteksi akhir animasi
+    this.renderer.listen(this.hireBtn.nativeElement, 'animationend', () => {
+      // Menghapus kelas animasi setelah animasi selesai
+      this.renderer.removeClass(this.hireBtn.nativeElement, 'animate__animated');
+      this.renderer.removeClass(this.hireBtn.nativeElement, 'animate__fadeInDown');
+      this.renderer.removeClass(this.hireBtn.nativeElement, 'animate__delay-2s');
+    });
+  }
+
+  // Fungsi untuk alert
+  showAlert() {
+    alert('Terima Kasih!!');
+  }
   
 }
